f7a9d531537066cc98deb1d1a1ae18debb4cd375
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-05-23 00:19:28 +0200

    feature: added state management

--- cli/src/main/java/com/resourcetracker/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.resourcetracker.constant;
-
-public class Constants {
-	public static final String HOME_FOLDER_PATH = "/usr/local/etc/";
-	public static final String CONFIG_FILE_NAME = "resourcetracker.yaml";
-}
