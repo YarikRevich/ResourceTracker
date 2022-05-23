62baf534db59511b3001bb9c7276ccef6f928ec7
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-05-23 17:35:59 +0200

    feature: added one subservice for saving terraform state inside tool

--- constants/src/main/java/com/resourcetracker/Constants.java
+++ constants/src/main/java/com/resourcetracker/Constants.java
@@ -3,6 +3,10 @@ package com.resourcetracker;
 public class Constants {
 	public static final String HOME_FOLDER_PATH = "/usr/local/etc/resourcetracker";
 
-	public static final String CONFIG_FILE_PATH = Strings.join(HOME_FOLDER_PATH, "resourcetracker.yaml");
-	public static final String STARTED_STATE_FILE_PATH = Strings.join(HOME_FOLDER_PATH, ".started");
+	public static final String CONFIG_FILE_PATH = HOME_FOLDER_PATH.concat("resourcetracker.yaml");
+
+	/**
+	 * Represent file which are used by state manager
+	 */
+	public static final String STATE_FILE_PATH = HOME_FOLDER_PATH.concat(".state");
 }
