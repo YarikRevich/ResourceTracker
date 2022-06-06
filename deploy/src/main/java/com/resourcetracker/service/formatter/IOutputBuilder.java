a36d678204f842e0e0e74a7bce494c70e9ee8534
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-06-06 21:02:38 +0200

    feature: added processing of requests

---
+++ deploy/src/main/java/com/resourcetracker/service/formatter/IOutputBuilder.java
@@ -0,0 +1,12 @@
+package com.resourcetracker.service.formatter;
+
+import java.util.HashMap;
+
+public interface IOutputBuilder {
+	void setOutputType(OutputType outputType);
+	void setHeader(String msg);
+	void setBody(String msg);
+	String format();
+
+	HashMap<String, String> getData();
+}
