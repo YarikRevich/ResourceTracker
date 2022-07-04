2912fe6dc8368113b74203b199f5261270733799
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-07-04 18:01:15 +0200

    feature!: added tests

--- kafka-service/src/main/java/com/resourcetracker/service/configuration/KafkaConfiguration.java
+++ kafka-service/src/main/java/com/resourcetracker/service/configuration/KafkaConfiguration.java
@@ -9,38 +9,11 @@ import java.util.Properties;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 
+import com.resourcetracker.service.configuration.common.KafkaConfigurationBuilder;
+
 public class KafkaConfiguration {
-
-	@Value("#{environment.RESOURCETRACKER_KAFKA_BOOTSTRAP_SERVER}")
-	private static String envBoostrapServer;
-
-	public static String bootrapServer;
-
-	public static String getBoostrapServer() {
-		return bootrapServer;
-	}
-
-	public static void setBoostrapServer(String bootrapServer) {
-		KafkaConfiguration.bootrapServer = bootrapServer;
-	}
-	/**
-	 * Loads configuration for Kafka from 'application.yml'
-	 * file depending on the module 'kafka-service' is used in
-	 *
-	 * @return Loaded properties file
-	 */
-	public static Properties getConfiguration() {
-		final Properties streamsConfiguration = new Properties();
-		if (envBoostrapServer != null){
-			streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, envBoostrapServer);
-		} else if (bootrapServer != null){
-			streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootrapServer);
-		} else {
-			throw new RuntimeException("no 'bootstrap-servers' set in 'KafkaConfiguration'");
-		}
-		// streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-		// streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-		return streamsConfiguration;
+	public static KafkaConfigurationBuilder builder(){
+		return new KafkaConfigurationBuilder();
 	}
 
 	public static Map<String, Object> convertPropsToMap(Properties props){
@@ -50,16 +23,13 @@ public class KafkaConfiguration {
 		}
 		return retMap;
 	}
-
-
 	// try {
 	// 	properties.put(AdminClientConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostName());
 	// } catch (UnknownHostException e){
 	// 	e.printStackTrace();
 	// }
 
-	// properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	// properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
 
 	// if (stateService.isKafkaBootstrapServer()){
 	// 	properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, stateService.getKafkaBootstrapServer());
--- kafka-service/src/main/java/com/resourcetracker/service/stream/StatusSplitStream.java
+++ kafka-service/src/main/java/com/resourcetracker/service/stream/StatusSplitStream.java
@@ -1,5 +1,7 @@
 package com.resourcetracker.service.stream;
 
+import java.util.Properties;
+
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -19,20 +21,28 @@ import com.resourcetracker.service.stream.common.Stream;
  * Stream, which processes 'status' topic
  */
 public class StatusSplitStream implements Stream{
+	private Properties props;
+
+	public void init(Properties props){
+		this.props = props;
+	}
+
 	public void run() {
 		final StreamsBuilder builder = new StreamsBuilder();
 		final KStream<String, StatusEntity> status = builder.stream(Constants.KAFKA_STATUS_TOPIC, Consumed.with(
 				Serdes.String(),
 				Serdes.serdeFrom(new JsonSerializer<>(), new JsonDeserializer<>(StatusEntity.class))));
-		status
-				.filter((k, v) -> v.getStatusType() == StatusType.FAILURE)
-				.to(Constants.KAFKA_STATUS_FAILURE_TOPIC);
-		status
-				.filter((k, v) -> v.getStatusType() == StatusType.SUCCESS)
-				.to(Constants.KAFKA_STATUS_SUCCESS_TOPIC);
-		final KafkaStreams streams = new KafkaStreams(builder.build(), KafkaConfiguration.getConfiguration());
+		// status
+		// 		.filter((k, v) -> v.getStatusType() == StatusType.FAILURE)
+		// 		.to(Constants.KAFKA_STATUS_FAILURE_TOPIC);
+		// status
+		// 		.filter((k, v) -> v.getStatusType() == StatusType.SUCCESS)
+		// 		.to(Constants.KAFKA_STATUS_SUCCESS_TOPIC);
+		final KafkaStreams streams = new KafkaStreams(builder.build(), this.props);
 
 		streams.cleanUp();
 		streams.start();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 	}
 }
