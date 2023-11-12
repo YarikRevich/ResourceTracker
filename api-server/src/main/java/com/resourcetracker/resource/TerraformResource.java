15027d3b6299503b06643f189084a3a641f14428
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2023-11-12 16:27:59 +0100

    feature: updated API Server to use native Quarkus Open API generation

--- api-server/src/main/java/com/resourcetracker/resource/TerraformResource.java
+++ api-server/src/main/java/com/resourcetracker/resource/TerraformResource.java
@@ -3,11 +3,13 @@ package com.resourcetracker.resource;
 import com.resourcetracker.api.TerraformResourceApi;
 import com.resourcetracker.exception.TerraformException;
 import com.resourcetracker.model.TerraformDeploymentApplication;
+import com.resourcetracker.model.TerraformDeploymentApplicationResult;
 import com.resourcetracker.model.TerraformDestructionApplication;
 import com.resourcetracker.service.terraform.TerraformService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.BadRequestException;
+import org.openapi.quarkus.openapi_yml.api.TerraformResourceApi;
 
 @ApplicationScoped
 public class TerraformResource implements TerraformResourceApi {
@@ -15,7 +17,9 @@ public class TerraformResource implements TerraformResourceApi {
     TerraformService terraformService;
 
     @Override
-    public void v1TerraformApplyPost(TerraformDeploymentApplication terraformDeploymentApplication) {
+    public TerraformDeploymentApplicationResult v1TerraformApplyPost(TerraformDeploymentApplication terraformDeploymentApplication) {
+        new TerraformDeploymentApplicationResult();
+        return null;
 //        terraformService.apply(terraformDeploymentApplication);
     }
 
